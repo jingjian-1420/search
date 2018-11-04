a7ae035b45598bc1fe5dfaab85eb2ef574a50603
wei <18530017458@163.com>
2018-11-05 00:12:37 +0800

    a

--- lucene/src/test/java/com/chen/LuceneTest.java
+++ lucene/src/test/java/com/chen/LuceneTest.java
@@ -6,6 +6,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.ParseException;
@@ -14,10 +15,14 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Description:
@@ -26,51 +31,59 @@ import java.io.IOException;
  */
 public class LuceneTest {
 
+    /**
+     * 创建索引
+     */
     @Test
-    public void test() throws IOException, ParseException {
-        //标准分词器
-        Analyzer analyzer = new StandardAnalyzer();
-
+    public void test() throws IOException {
         //将索引存储在内存中：
-        Directory directory = new RAMDirectory();
+//        Directory directory = new RAMDirectory();
         //若要在磁盘上存储索引，请使用以下方法:
-        //Directory directory = FSDirectory.open("/tmp/testindex");
+        Path path = Paths.get("D:", "Lucene", "test","index");
+        Directory director1y = FSDirectory.open(path);
 
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        //默认使用 StandardAnalyzer 标准分词器
+        IndexWriterConfig indexWriterConfig = new IndexWriterConfig();
 
-        IndexWriter iwriter = new IndexWriter(directory, config);
-
+        //值 词法分析-语言处理-语法分析
+        IndexWriter indexWriter = new IndexWriter(director1y,indexWriterConfig);
 
         Document doc = new Document();
-        String text = "This is the text to be indexed.";
-        doc.add(new Field("fieldname", text, TextField.TYPE_STORED));
-        iwriter.addDocument(doc);
-        iwriter.close();
+        doc.add(new Field("name","wo shi shui, ni hao ya?",TextField.TYPE_STORED));
 
-        //现在搜索索引:
-        DirectoryReader ireader = DirectoryReader.open(directory);
-        IndexSearcher isearcher = new IndexSearcher(ireader);
+        indexWriter.addDocument(doc);
 
-        //解析搜索“text”的简单查询：
-        QueryParser parser = new QueryParser("fieldname", analyzer);
-        Query query = parser.parse("text");
-        ScoreDoc[] hits = isearcher.search(query,1000).scoreDocs;
-
-//        assertEquals(1, hits.length);
-        //迭代结果:
-        for (int i = 0; i < hits.length; i++) {
-            Document hitDoc = isearcher.doc(hits[i].doc);
-//            assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
-        }
-        ireader.close();
-        directory.close();
+        indexWriter.close();
+        director1y.close();
     }
 
     /**
-     * 分词
+     * 搜索索引
      */
     @Test
-    public void test1(){
+    public void test1() throws IOException, ParseException {
+        Path path = Paths.get("D:", "Lucene", "test","index");
 
+        FSDirectory directory = FSDirectory.open(path);
+        //读索引
+        IndexReader indexReader = DirectoryReader.open(directory);
+        //搜索索引
+        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+
+
+        //标准分词器
+        Analyzer analyzer = new StandardAnalyzer();
+
+        //搜索 含有wo 的文档
+        QueryParser parser = new QueryParser("name", analyzer);
+        Query query = parser.parse("wo");
+        ScoreDoc[] hits = indexSearcher.search(query,1000).scoreDocs;
+
+        //迭代结果:
+        for (int i = 0; i < hits.length; i++) {
+            Document hitDoc = indexSearcher.doc(hits[i].doc);
+        }
+        indexReader.close();
+        directory.close();
     }
 }
